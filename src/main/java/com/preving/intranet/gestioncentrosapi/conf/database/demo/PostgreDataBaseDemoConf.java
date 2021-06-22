76b80e3ba65b6c2effce84f01c25fb420f836107
Pablo Plasencia <p.plasencia@preving.com>
2021-06-22 11:30:10 +0200

    UPDATED: Eliminar bean de transacciones manuales para demo

--- src/main/java/com/preving/intranet/gestioncentrosapi/conf/database/demo/PostgreDataBaseDemoConf.java
+++ src/main/java/com/preving/intranet/gestioncentrosapi/conf/database/demo/PostgreDataBaseDemoConf.java
@@ -8,7 +8,6 @@ import org.springframework.context.annotation.Profile;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
-import org.springframework.jdbc.datasource.DataSourceTransactionManager;
 import org.springframework.jdbc.datasource.lookup.JndiDataSourceLookup;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
@@ -39,11 +38,6 @@ public class PostgreDataBaseDemoConf {
     @Bean(name = "namedTemplatePostgresql")
     public NamedParameterJdbcTemplate namedTemplateApp(@Qualifier("postgresqlDataSource") DataSource dsApp) {
         return new NamedParameterJdbcTemplate(dsApp);
-    }
-
-    @Bean(name= "manualTransactionManagerPostgresql")
-    public DataSourceTransactionManager manualTransactionManagerPostgresql(@Qualifier("postgresqlDataSource") DataSource dsApp) {
-        return new DataSourceTransactionManager(dsApp);
     }
 
 }
