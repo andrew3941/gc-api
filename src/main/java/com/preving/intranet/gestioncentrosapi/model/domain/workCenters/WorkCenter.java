e3266a3278340d25be8caf189aa2954df57772bd
Pablo Plasencia <p.plasencia@preving.com>
2021-07-01 09:44:04 +0200

    UPDATED: Tipo Integer para lineaId en el dominio de WorkCenter.

--- src/main/java/com/preving/intranet/gestioncentrosapi/model/domain/workCenters/WorkCenter.java
+++ src/main/java/com/preving/intranet/gestioncentrosapi/model/domain/workCenters/WorkCenter.java
@@ -52,7 +52,7 @@ public class WorkCenter implements Serializable {
     @JsonFormat(pattern = "yyyy-MM-dd", timezone = "Europe/Madrid")
     private Date endDate = null;
     private int idInMp2;
-    private int lineId;
+    private Integer lineId;
     private com.preving.intranet.gestioncentrosapi.model.domain.Entity entity;
     private int active;
     private int visible;
@@ -86,7 +86,7 @@ public class WorkCenter implements Serializable {
 
     public WorkCenter(int id, String name, City city, String navisionCode, String address, String postalCode,
                       String phoneNumber, String email, User headPerson, Date startDate, Date endDate, int idInMp2,
-                      int lineId, com.preving.intranet.gestioncentrosapi.model.domain.Entity entity, int active,
+                      Integer lineId, com.preving.intranet.gestioncentrosapi.model.domain.Entity entity, int active,
                       int visible, Date created, User createdBy, Date modified, User modifiedBy) {
         this.id = id;
         this.name = name;
@@ -224,10 +224,10 @@ public class WorkCenter implements Serializable {
 
     @Basic
     @Column(name = "LINEA_ID")
-    public int getLineId() {
+    public Integer getLineId() {
         return lineId;
     }
-    public void setLineId(int lineId) {
+    public void setLineId(Integer lineId) {
         this.lineId = lineId;
     }
 
