a6efdefbe8c4d06ae4376dcddd586890eebb9d4e
alhaji.kanu <a.kanu@ubuntuafrika.com>
2021-07-06 09:55:22 +0000

    UPDATE: Extend the jpaRepostory for workcenterdetailsReporstory

--- src/main/java/com/preving/intranet/gestioncentrosapi/model/dao/workCenters/WorkCenterDetailsRepository.java
+++ src/main/java/com/preving/intranet/gestioncentrosapi/model/dao/workCenters/WorkCenterDetailsRepository.java
@@ -1,8 +1,9 @@
 package com.preving.intranet.gestioncentrosapi.model.dao.workCenters;
 
 import com.preving.intranet.gestioncentrosapi.model.domain.workCenters.WorkCenterDetails;
+import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface WorkCenterDetailsRepository {
+public interface WorkCenterDetailsRepository extends JpaRepository<WorkCenterDetails, Integer> {
 
     WorkCenterDetails findWorkCenterDetails(int workCenterId);
 }
